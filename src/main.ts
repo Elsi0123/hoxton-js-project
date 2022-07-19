a8bec5355cee19de4022fc709143c9798045cff8
Teodora Gjini <teodoragjini@yahoo.com>
2022-07-19 02:34:33 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,48 @@
-import './style.css'
+import "./style.css";
 
 let songs = [
-      {
-        "id": 1,
-        "title": "Song 1",
-        "genre":"",
-        "thumbnail": "",
-        "src": "https://github.com/lucas-santosP/clone-spotify-web-react/raw/master/src/assets/demo-preview.png",
-        "author": ""
-      },
+  {
+    id: 1,
+    title: "Song 1",
+    genre: "",
+    thumbnail: "",
+    src: "https://github.com/lucas-santosP/clone-spotify-web-react/raw/master/src/assets/demo-preview.png",
+    author: "",
+  },
 
-      {
-        "id": 2,
-        "title": "Song 2",
-        "genre":"",
-        "thumbnail": "",
-        "src": "https://github.com/lucas-santosP/clone-spotify-web-react/raw/master/src/assets/demo-preview.png",
-        "author": ""
-      }
-    ]
+  {
+    id: 2,
+    title: "Song 2",
+    genre: "",
+    thumbnail: "",
+    src: "https://github.com/lucas-santosP/clone-spotify-web-react/raw/master/src/assets/demo-preview.png",
+    author: "",
+  },
+];
 
-  let songsContainer = document.getElementById('songs')
+let songsContainer = document.getElementById("songs");
 
-  function renderSongs(){
-    for(let song of songs){
-        let divItem = document.createElement('div')
+function renderSongs() {
+  for (let song of songs) {
+    let divItem = document.createElement("div");
 
-        let imgItem = document.createElement('img')
-        imgItem.src = song.src
-        imgItem.width = 250
+    let imgItem = document.createElement("img");
+    imgItem.src = song.src;
+    imgItem.width = 250;
 
-        let h2Tittle = document.createElement('h2')
-        h2Tittle.textContent = song.title
+    let h2Tittle = document.createElement("h2");
+    h2Tittle.textContent = song.title;
 
-        let h3Author = document.createElement('h3')
-        h3Author.textContent = song.author
+    let h3Author = document.createElement("h3");
+    h3Author.textContent = song.author;
 
-        let spanFavorite = document.createElement('span')
-        spanFavorite.className = "material-symbols-outlined"
-        spanFavorite.textContent = 'favorite'
+    let spanFavorite = document.createElement("span");
+    spanFavorite.className = "material-symbols-outlined";
+    spanFavorite.textContent = "favorite";
 
-        divItem.append(imgItem,h2Tittle,h3Author,spanFavorite)
-
-
-        songsContainer?.append(divItem)
-    }  
+    divItem.append(imgItem, h2Tittle, h3Author, spanFavorite);
+    songsContainer?.append(divItem);
   }
+}
 
-renderSongs()
+renderSongs();
